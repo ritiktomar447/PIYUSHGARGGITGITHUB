3179117cb02e1773d1e282a793e0d72f527ec98a
Ritik Tomar <ritiktomar468@gmail.com>
2025-04-28 12:57:53 +0530

    Added isAbove18 fn

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const age=24
-const name="ritik tomar"
+const name1="ritik tomar"
 const dob="10/10/2001"
  function name() {
   console.log("my name is"+name)
 }
 const a=15;
 const b=15;
+function isAbove{
+  return age>=18
+}
